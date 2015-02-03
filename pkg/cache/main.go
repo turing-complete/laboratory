5d26f392e6de67ebcfe500d0404f57e3acb0c4f9
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-02-03 10:25:56 +0100

    Got rid of defer in pkg/cache

--- pkg/cache/main.go
+++ pkg/cache/main.go
@@ -17,13 +17,13 @@ func New(capacity uint32) *Cache {
 
 func (c *Cache) Get(key string) (value interface{}, ok bool) {
 	c.mutex.Lock()
-	defer c.mutex.Unlock()
 	value, ok = c.mapping[key]
+	c.mutex.Unlock()
 	return
 }
 
 func (c *Cache) Add(key string, value interface{}) {
 	c.mutex.Lock()
-	defer c.mutex.Unlock()
 	c.mapping[key] = value
+	c.mutex.Unlock()
 }
