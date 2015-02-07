aa306f16779a513953d6af0c4ed2c35e45f3f028
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-02-07 07:55:24 +0100

    Go fmt!

--- cmd/compare/main.go
+++ cmd/compare/main.go
@@ -47,7 +47,7 @@ func command(config internal.Config, input *mat.File, _ *mat.File) error {
 
 	fmt.Printf("Inputs: %2d, outputs: %4d, level: %2d, nodes: %7d, μ %.2e, σ %.2e, Δ %.2e\n",
 		surrogate.Inputs, surrogate.Outputs, surrogate.Level, surrogate.Nodes,
-		math.Abs((μ1 - μ2) / μ1), math.Abs((σ1 - σ2) / σ1), Δ)
+		math.Abs((μ1-μ2)/μ1), math.Abs((σ1-σ2)/σ1), Δ)
 
 	return nil
 }
