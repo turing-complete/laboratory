3e8b26d8c4f886dfa943823bb7b3fc62ab708487
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-02-13 15:52:51 +0100

    Updated the usage of numeric/*

--- cmd/internal/interpolator.go
+++ cmd/internal/interpolator.go
@@ -11,21 +11,22 @@ import (
 
 func NewInterpolator(problem *Problem, target Target) (*adhier.Interpolator, error) {
 	config := &problem.Config.Interpolation
-	ic, oc := target.Inputs(), target.Outputs()
+	ic, oc := uint16(target.Inputs()), uint16(target.Outputs())
 
 	var grid adhier.Grid
 	var basis adhier.Basis
 
 	switch strings.ToLower(config.Rule) {
 	case "open":
-		grid = newcot.NewOpen(uint16(ic))
-		basis = linhat.NewOpen(uint16(ic), uint16(oc))
+		grid, basis = newcot.NewOpen(ic), linhat.NewOpen(ic)
 	case "closed":
-		grid = newcot.NewClosed(uint16(ic))
-		basis = linhat.NewClosed(uint16(ic), uint16(oc))
+		grid, basis = newcot.NewClosed(ic), linhat.NewClosed(ic)
 	default:
 		return nil, errors.New("the interpolation rule is unknown")
 	}
+
+	// FIXME: Altering the problem’s data might not be a good idea.
+	config.Inputs, config.Outputs = ic, oc
 
 	return adhier.New(grid, basis, (*adhier.Config)(&config.Config))
 }
