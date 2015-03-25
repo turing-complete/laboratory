e8b43cc800b545e912cae985e4738d510dee6ee9
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-03-25 14:43:30 +0100

    Add a new command cmd/sweep

---
+++ cmd/sweep/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"runtime"
+
+	"github.com/ready-steady/format/mat"
+
+	"../internal"
+)
+
+var numberOfPoints = flag.Uint("number", 100, "the number of points per dimension")
+
+func main() {
+	internal.Run(command)
+}
+
+func command(config internal.Config, _ *mat.File, output *mat.File) error {
+	problem, err := internal.NewProblem(config)
+	if err != nil {
+		return err
+	}
+
+	target, err := internal.NewTarget(problem)
+	if err != nil {
+		return err
+	}
+
+	points := generate(target)
+
+	if config.Verbose {
+		fmt.Println("Sweeping the reduced model...")
+		fmt.Println(problem)
+		fmt.Println(target)
+	}
+
+	values := internal.Invoke(target, points, uint(runtime.GOMAXPROCS(0)))
+
+	if config.Verbose {
+		fmt.Println("Done.")
+	}
+
+	if output == nil {
+		return nil
+	}
+
+	ni, no := target.Dimensions()
+	np := uint(len(points)) / ni
+
+	if err := output.PutArray("values", values, no, np); err != nil {
+		return err
+	}
+	if err := output.PutArray("points", points, ni, np); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func generate(target internal.Target) []float64 {
+	np := *numberOfPoints
+	ni, _ := target.Dimensions()
+
+	points := make([]float64, np*ni)
+
+	for i := uint(0); i < np; i++ {
+		value := float64(i) * 1.0 / float64(np-1)
+		for j := uint(0); j < ni; j++ {
+			points[i*ni+j] = value
+		}
+	}
+
+	return points
+}
