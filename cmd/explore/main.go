e6d2456118a3d331c1a1af5426f1834bcd96e9e4
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-04-24 11:53:23 +0200

    cmd/explore: print the problem and target

--- cmd/explore/main.go
+++ cmd/explore/main.go
@@ -50,6 +50,8 @@ func command(config *internal.Config) error {
 	np := uint(len(points)) / ni
 
 	if config.Verbose {
+		fmt.Println(problem)
+		fmt.Println(target)
 		fmt.Printf("Evaluating the model with reduction %.2f at %v points...\n",
 			config.Probability.VarThreshold, np)
 	}
