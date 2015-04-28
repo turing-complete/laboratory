d1d4e9be7bcb3b2fc9061b4be6c63cd98841bed6
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-04-28 07:29:25 +0200

    analysis/approximate: check the admissibility condition

--- run/analysis/approximate.m
+++ run/analysis/approximate.m
@@ -5,13 +5,34 @@ function approximate
   solution = h5read(filename, '/solution');
   surrogate = solution.Surrogate;
 
+  ni = double(surrogate.Inputs);
   no = double(surrogate.Outputs);
-  surpluses = reshape(surrogate.Surpluses{1}, no, []);
+  indices = reshape(surrogate.Indices{1}, ni, [])';
+  surpluses = reshape(surrogate.Surpluses{1}, no, [])';
 
   Plot.figure(1200, 400);
   for i = 1:no
     subplot(no, 1, i);
-    semilogy(abs(surpluses(i, :)));
-	Plot.title('Output %d', i-1);
+    semilogy(abs(surpluses(:, i)));
+    Plot.title('Output %d', i-1);
+  end
+
+  good = admissibility(indices);
+  if any(~good)
+    warning('found %d inadmissible indices out of %d', sum(~good), length(good));
+  end
+end
+
+function good = admissibility(indices)
+  [nn, ni] = size(indices);
+
+  good = false(nn, 1);
+  indices = bitshift(bitshift(indices, 32), -32);
+  indices = int32(indices);
+
+  for i = 1:nn
+    I = repmat(indices(i, :), ni, 1) - int32(eye(ni));
+    I(any(I < 0, 1), :) = [];
+    good(i) = all(ismember(I, indices, 'rows'));
   end
 end
