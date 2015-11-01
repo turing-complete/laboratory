c925526df4ddb8162ecec49e8c61b5e0828f103d
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-11-01 10:14:40 +0100

    i/uncertainty: a bit of clean-up

--- src/internal/uncertainty/marginal.go
+++ src/internal/uncertainty/marginal.go
@@ -1,23 +1,53 @@
 package uncertainty
 
 import (
+	"errors"
+	"fmt"
+	"math"
+
+	"github.com/ready-steady/linear/matrix"
 	"github.com/ready-steady/probability"
+	"github.com/ready-steady/statistics/correlation"
 	"github.com/turing-complete/laboratory/src/internal/config"
 	"github.com/turing-complete/laboratory/src/internal/distribution"
+	"github.com/turing-complete/laboratory/src/internal/support"
 	"github.com/turing-complete/laboratory/src/internal/system"
+
+	icorrelation "github.com/turing-complete/laboratory/src/internal/correlation"
 )
 
-type Marginal struct {
-	base
-	marginals []probability.Inverter
-	reference []float64
+var (
+	standardGaussian = probability.NewGaussian(0, 1)
+	nInfinity        = math.Inf(-1)
+	pInfinity        = math.Inf(1)
+)
+
+type marginal struct {
+	taskIndex  []uint
+	correlator []float64
+	marginals  []probability.Inverter
+	reference  []float64
+
+	nt uint
+	nu uint
+	nz uint
 }
 
-func newMarginal(s *system.System, c *config.Uncertainty) (*Marginal, error) {
-	base, err := newBase(s, c)
+func newMarginal(s *system.System, c *config.Uncertainty) (*marginal, error) {
+	nt := uint(s.Application.Len())
+
+	taskIndex, err := support.ParseNaturalIndex(c.TaskIndex, 0, nt-1)
 	if err != nil {
 		return nil, err
 	}
+
+	correlator, err := correlate(s, c, taskIndex)
+	if err != nil {
+		return nil, err
+	}
+
+	nu := uint(len(taskIndex))
+	nz := uint(len(correlator)) / nu
 
 	marginalizer, err := distribution.ParseInverter(c.Marginal)
 	if err != nil {
@@ -25,26 +55,117 @@ func newMarginal(s *system.System, c *config.Uncertainty) (*Marginal, error) {
 	}
 
 	reference := s.ReferenceTime()
-	marginals := make([]probability.Inverter, base.nu)
-	for i, tid := range base.taskIndex {
+	marginals := make([]probability.Inverter, nu)
+	for i, tid := range taskIndex {
 		marginals[i] = marginalizer(0, c.MaxDelay*reference[tid])
 	}
 
-	return &Marginal{
-		base:      *base,
-		marginals: marginals,
-		reference: reference,
+	return &marginal{
+		taskIndex:  taskIndex,
+		correlator: correlator,
+		marginals:  marginals,
+		reference:  reference,
+
+		nt: nt,
+		nu: nu,
+		nz: nz,
 	}, nil
 }
 
-func (m *Marginal) Transform(z []float64) []float64 {
-	u := m.base.Transform(z)
+func (m *marginal) Transform(z []float64) []float64 {
+	nt, nu, nz := m.nt, m.nu, m.nz
 
-	duration := make([]float64, m.nt)
-	copy(duration, m.reference)
-	for i, tid := range m.taskIndex {
-		duration[tid] += m.marginals[i].InvCDF(standardGaussian.CDF(u[i]))
+	n := make([]float64, nz)
+	u := make([]float64, nu)
+	d := make([]float64, nt)
+
+	// Independent uniform to independent Gaussian
+	for i := range n {
+		n[i] = standardGaussian.InvCDF(z[i])
 	}
 
-	return duration
+	// Independent Gaussian to dependent Gaussian
+	multiply(m.correlator, n, u, nu, nz)
+
+	// Dependent Gaussian to dependent uniform
+	for i := range u {
+		u[i] = standardGaussian.CDF(u[i])
+	}
+
+	// Dependent uniform to dependent desired
+	copy(d, m.reference)
+	for i, tid := range m.taskIndex {
+		d[tid] += m.marginals[i].InvCDF(standardGaussian.CDF(u[i]))
+	}
+
+	return d
+}
+
+func (m *marginal) Len() int {
+	return int(m.nz)
+}
+
+func (m *marginal) String() string {
+	return fmt.Sprintf(`{"parameters": %d, "variables": %d}`, m.nu, m.nz)
+}
+
+func correlate(s *system.System, c *config.Uncertainty, taskIndex []uint) ([]float64, error) {
+	if c.CorrLength < 0 {
+		return nil, errors.New("the correlation length should be nonnegative")
+	}
+	if c.VarThreshold <= 0 {
+		return nil, errors.New("the variance-reduction threshold should be positive")
+	}
+
+	if c.CorrLength == 0 {
+		return matrix.Identity(uint(len(taskIndex))), nil
+	}
+
+	C := icorrelation.Compute(s.Application, taskIndex, c.CorrLength)
+	correlator, _, err := correlation.Decompose(C, uint(len(taskIndex)), c.VarThreshold)
+	if err != nil {
+		return nil, err
+	}
+
+	return correlator, nil
+}
+
+func multiply(A, x, y []float64, m, n uint) {
+	infinite, z := false, make([]float64, n)
+
+	for i := range x {
+		switch x[i] {
+		case nInfinity:
+			infinite, z[i] = true, -1
+		case pInfinity:
+			infinite, z[i] = true, 1
+		}
+	}
+
+	if !infinite {
+		matrix.Multiply(A, x, y, m, n, 1)
+		return
+	}
+
+	for i := uint(0); i < m; i++ {
+		Σ1, Σ2 := 0.0, 0.0
+		for j := uint(0); j < n; j++ {
+			a := A[j*m+i]
+			if a == 0 {
+				continue
+			}
+			if z[j] == 0 {
+				Σ1 += a * x[j]
+			} else {
+				Σ2 += a * z[j]
+			}
+		}
+		if Σ2 < 0 {
+			y[i] = nInfinity
+		} else if Σ2 > 0 {
+			y[i] = pInfinity
+		} else {
+			y[i] = Σ1
+		}
+	}
 }
--- src/internal/uncertainty/marginal_test.go
+++ src/internal/uncertainty/marginal_test.go
@@ -1,6 +1,7 @@
 package uncertainty
 
 import (
+	"math"
 	"testing"
 
 	"github.com/ready-steady/assert"
@@ -37,4 +38,28 @@ func TestNewMarginal002(t *testing.T) {
 		0.0020, 0.0006, 0.0076, 0.0062, 0.0004, 0.0038, 0.0006, 0.0062, 0.0036, 0.0056,
 		0.0038, 0.0010, 0.0068, 0.0070, 0.0078, 0.0044, 0.0004, 0.0058, 0.0056, 0.0040,
 	}, 1e-15, t)
+}
+
+func TestMultiply(t *testing.T) {
+	m, n := uint(4), uint(3)
+
+	A := []float64{
+		+0, +1, -2, +0,
+		-1, -2, +0, +1,
+		+1, +1, +0, +2,
+	}
+
+	test := func(x, y []float64) {
+		z := make([]float64, m)
+		multiply(A, x, z, m, n)
+		assert.Equal(z, y, t)
+	}
+
+	inf := math.Inf(1)
+
+	test([]float64{1, 2, 1}, []float64{-1, -2, -2, 4})
+	test([]float64{inf, 2, 1}, []float64{-1, inf, -inf, 4})
+	test([]float64{1, -inf, 1}, []float64{inf, inf, -2, -inf})
+	test([]float64{1, 2, inf}, []float64{inf, inf, -2, inf})
+	test([]float64{inf, 2, -inf}, []float64{-inf, -4, -inf, -inf})
 }
