07a45494667cd9a447ec9c981b26cf08c0d8afd2
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-11-02 06:31:54 +0100

    a/observe: plot distributions

--- run/analysis/observe.m
+++ run/analysis/observe.m
@@ -1,13 +1,20 @@
 function observe()
+  use('Interaction');
+
   filename = locate('observe');
   values = h5read(filename, '/values');
   values = values(1:2:end, :);
 
   [no, ns] = size(values);
 
-  I = randi(ns, 1, min(10, ns));
-
   figure;
+  I = randi(ns, 1, min(10, ns));
   plot(values(:, I), 'Marker', 'o');
-  title('Observed samples');
+  Plot.title('Observed samples');
+
+  for i = 1:no
+    figure;
+    hist(values(i, :), 100);
+    Plot.title('Output %d', i);
+  end
 end
