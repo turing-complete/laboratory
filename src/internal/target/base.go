75ee97db5aa01f5632e3756b8aa6428f20f2b052
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-04-12 08:42:32 +0200

    i/target: remove leftovers

--- src/internal/target/base.go
+++ src/internal/target/base.go
@@ -2,13 +2,9 @@ package target
 
 import (
 	"fmt"
-	"log"
-	"math"
 
 	"github.com/turing-complete/laboratory/src/internal/config"
 	"github.com/turing-complete/laboratory/src/internal/system"
-
-	interpolation "github.com/ready-steady/adapt/algorithm/external"
 )
 
 type base struct {
@@ -17,9 +13,6 @@ type base struct {
 
 	ni uint
 	no uint
-
-	ns uint
-	nn uint
 }
 
 func newBase(system *system.System, config *config.Target, ni, no uint) (base, error) {
@@ -28,27 +21,6 @@ func newBase(system *system.System, config *config.Target, ni, no uint) (base, e
 
 func (self *base) Dimensions() (uint, uint) {
 	return self.ni, self.no
-}
-
-func (self *base) Check(state *interpolation.State, _ *interpolation.Surrogate) {
-	if self.ns == 0 {
-		log.Printf("%5s %15s %15s\n", "", "Done", "More")
-	}
-
-	nn := uint(len(state.Indices)) / self.ni
-
-	log.Printf("%5d %15d %15d\n", self.ns, self.nn, nn)
-
-	self.ns += 1
-	self.nn += nn
-}
-
-func (self *base) Score(element *interpolation.Element) (score float64) {
-	for _, value := range element.Surplus {
-		score += math.Abs(value)
-	}
-	score *= element.Volume
-	return
 }
 
 func (self *base) String() string {
